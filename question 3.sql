7228da2baa910938281da74e4881383af425cf56
RajeshPalani-98 <156658476+RajeshPalani-98@users.noreply.github.com>
2024-01-26 12:34:25 +0530

    Add files via upload

--- question 3.sql
+++ question 3.sql
@@ -1,16 +1,7 @@
-ALTER TABLE	fact_events
-ADD COLUMN Revenue_before_promo int,
-ADD COLUMN Revenue_after_promo int;
-
-UPDATE fact_events 
-SET 
-    Revenue_before_promo = base_price * `quantity_sold(before_promo)`,
-    Revenue_after_promo = base_price * `quantity_sold(after_promo)`;
-    
 SELECT 
     campaign_name,
-    concat((SUM(Revenue_before_promo)/ 1000000.0),' M') as Total_Revenue_before_promo,
-    concat((SUM(Revenue_after_promo) / 1000000.0),' M') as Total_Revenue_after_promo
+    concat(round(SUM(Revenue_before_promo)/ 1000000.0),' M') as Total_Revenue_before_promo,
+    concat(round(SUM(Revenue_after_promo) / 1000000.0),' M') as Total_Revenue_after_promo
 FROM
     fact_events AS fe
         JOIN
